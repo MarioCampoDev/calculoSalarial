bdb58b4b11d12a6989272cf07f92357b00e57e3f
Mario Alberto Campo Londoño <mariocampo.dev@gmail.com>
2024-09-22 01:00:29 -0500

    calculo de salario de empleado

--- src/main/java/com/mycompany/calculosalarial/CalculoSalarial.java
+++ src/main/java/com/mycompany/calculosalarial/CalculoSalarial.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.calculosalarial;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mario
@@ -11,6 +13,31 @@ package com.mycompany.calculosalarial;
 public class CalculoSalarial {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int sueldoBaseRepositor;
+        double sueldoBaseCajero, sueldoBaseSupervisor, sueldoFinal = 0;
+        Scanner entradaTeclado = new Scanner (System.in);
+        System.out.println("Ingrese el empleado: ");
+        String categoriaSueldo = entradaTeclado.nextLine().toLowerCase();
+        switch (categoriaSueldo) {
+            case "repositor": 
+                sueldoBaseRepositor = 15890;
+                sueldoFinal = sueldoBaseRepositor + (sueldoBaseRepositor * 0.10);
+                break;
+            case "cajero":
+                sueldoBaseCajero = 25630.89;
+                sueldoFinal = sueldoBaseCajero;
+                break;
+            case "supervisor":
+                sueldoBaseSupervisor = 35560.20;
+                sueldoFinal = sueldoBaseSupervisor - (sueldoBaseSupervisor * 0.11);
+                break;
+            default:
+                System.out.println("La categoria de empleado no existe");
+                break;
+        }
+        
+        if (sueldoFinal != 0) {
+            System.out.println("El sueldo del "+categoriaSueldo+" es de: "+sueldoFinal);
+        }
     }
 }
