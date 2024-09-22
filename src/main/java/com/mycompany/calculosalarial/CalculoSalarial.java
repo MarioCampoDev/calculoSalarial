30b95ff55dd73f7c50f0bfdb1dd9729092502083
Mario Alberto Campo Londoño <mariocampo.dev@gmail.com>
2024-09-22 00:57:12 -0500

    initial commit

---
+++ src/main/java/com/mycompany/calculosalarial/CalculoSalarial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculosalarial;
+
+/**
+ *
+ * @author mario
+ */
+public class CalculoSalarial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
